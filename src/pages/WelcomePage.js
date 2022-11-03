3c00112e4c1007a690b1b24b2753a2c484f95777
Theoutrace <prakashkumar.imw@gmail.com>
2022-11-04 01:57:46 +0530

    Email verification done

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import AuthContext from "../Store/auth/auth-context";
 import "./WelcomePage.css";
 
 const WelcomePage = () => {
+  const authCtx=useContext(AuthContext)
+  // const [emailVfied, setEmailVfied] = useState('')
+  // // console.log(authCtx);
   const history = useNavigate()
   const completeProfileNowhandler = ()=>{
     history('/profile')
   }
+
+
+
+  const verifyMailHandler = ()=>{
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyC4NwKi-WNuGvMdl2_U3M7motBl31iKQO4',{
+      method: 'POST',
+      body: JSON.stringify({
+        requestType: "VERIFY_EMAIL",
+        idToken: authCtx.token
+      }),
+      headers: {'Content-Type': 'application/json'}
+    }).then(res=>{
+      if(res.ok){
+        return res.json().then(data=>{
+          console.log(data);
+          // setEmailVfied(data.email)
+        })
+      }else{
+        const errorMsg = 'email verification failed'
+        alert(errorMsg)
+      }
+    })
+  }
+
+
   return (
+    <Fragment>
     <div className="welcome-component">
       <div className="welcome-txt-welcome-page">Welcome to Expense Tracker</div>
       <div className="profile-complete-txt">
         Your profile is incomplete.<button onClick={completeProfileNowhandler}>Complete now</button>
       </div>
     </div>
+    <div className="under-welcome-msg-component-verify-btn-container">
+        <button onClick={verifyMailHandler}>Verify Your Email</button>
+      </div>
+    </Fragment>
   );
 };
 
